5754fd6d193f06730ee19a41e47f20773d9b14c8
Sean Hammell <sean.m.hammell@gmail.com>
2023-05-01 10:35:58 -0500

    Smooth out paddle controls

--- src/entity/input_component.cc
+++ src/entity/input_component.cc
@@ -5,18 +5,27 @@
 #include "src/entity/paddle.h"
 
 void InputComponent::Update(Paddle *paddle, SDL_Event input) {
-  if (input.type == SDL_KEYDOWN) {
+  if (input.type == SDL_KEYDOWN && input.key.repeat == 0) {
     switch (input.key.keysym.sym) {
       case SDLK_LEFT:
-        paddle->velocity_ = -kPaddleVelocity;
+        paddle->velocity_ -= kPaddleVelocity;
         break;
       case SDLK_RIGHT:
-        paddle->velocity_ = kPaddleVelocity;
+        paddle->velocity_ += kPaddleVelocity;
         break;
       default:
         break;
     }
   } else if (input.type == SDL_KEYUP) {
-    paddle->velocity_ = 0;
+    switch (input.key.keysym.sym) {
+      case SDLK_LEFT:
+        paddle->velocity_ += kPaddleVelocity;
+        break;
+      case SDLK_RIGHT:
+        paddle->velocity_ -= kPaddleVelocity;
+        break;
+      default:
+        break;
+    }
   }
 }
