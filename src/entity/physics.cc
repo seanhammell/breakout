7930432e46145381100d148f09fa3651455c86bf
Sean Hammell <sean.m.hammell@gmail.com>
2023-05-08 20:14:17 -0500

    Move collision check to seperate function

--- src/entity/physics.cc
+++ src/entity/physics.cc
@@ -21,15 +21,19 @@ void Physics::Update(Ball *ball, const Paddle& paddle) {
   ApplyVelocity(ball, ball->x_vel_, ball->y_vel_);
 }
 
-bool Physics::Intersection(const Line& a, const Line& b, Point *vertex) {
+double Physics::Distance(const Point& a, const Point& b) {
+  return std::sqrt(std::pow(b.x - a.x, 2) + std::pow(b.y - a.y, 2));
+}
+
+bool Physics::Intersection(const Line& a, const Line& b) {
   int denom{ ((b.y2 - b.y1) * (a.x2 - a.x1) - (b.x2 - b.x1) * (a.y2 - a.y1)) };
   int num_a{ ((b.x2 - b.x1) * (a.y1 - b.y1) - (b.y2 - b.y1) * (a.x1 - b.x1)) };
   int num_b{ ((a.x2 - a.x1) * (a.y1 - b.y1) - (a.y2 - a.y1) * (a.x1 - b.x1)) };
 
   if (denom == 0) {
     if (num_a == 0 && num_b == 0) {
-      vertex->x = (a.x1 + a.x2) / 2;
-      vertex->y = (a.y1 + a.y2) / 2;
+      vertex_.x = (a.x1 + a.x2) / 2;
+      vertex_.y = (a.y1 + a.y2) / 2;
       return true;
     }
 
@@ -43,9 +47,20 @@ bool Physics::Intersection(const Line& a, const Line& b, Point *vertex) {
     return false;
   }
 
-  vertex->x = a.x1 + ua * (a.x2 - a.x1);
-  vertex->y = a.y1 + ua * (a.y2 - a.y1);
+  vertex_.x = a.x1 + ua * (a.x2 - a.x1);
+  vertex_.y = a.y1 + ua * (a.y2 - a.y1);
   return true;
+}
+
+void Physics::CheckCollision(const Line& bound, Collision type) {
+  if (Intersection(path_, bound)) {
+    if (Distance(origin_, vertex_) < nearest_vertex_) {
+      path_.x2 = vertex_.x;
+      path_.y2 = vertex_.y;
+      nearest_vertex_ = Distance(origin_, vertex_);
+      surface_ = type;
+    }
+  }
 }
 
 void Physics::ApplyVelocity(Ball *ball, int x_velocity, int y_velocity) {
@@ -53,33 +68,25 @@ void Physics::ApplyVelocity(Ball *ball, int x_velocity, int y_velocity) {
     return;
   }
 
-  Line path{ };
-  Point vertex{ };
-  Collision surface{ kNone };
+  origin_.x = x_velocity < 0 ? ball->x_pos_ : ball->x_pos_ + Ball::kBallWidth;
+  origin_.y = y_velocity < 0 ? ball->y_pos_ : ball->y_pos_ + Ball::kBallHeight;
+  vertex_.x = origin_.x + x_velocity;
+  vertex_.y = origin_.y + y_velocity;
 
-  path.x1 = x_velocity < 0 ? ball->x_pos_ : ball->x_pos_ + Ball::kBallWidth;
-  path.y1 = y_velocity < 0 ? ball->y_pos_ : ball->y_pos_ + Ball::kBallHeight;
-  path.x2 = path.x1 + x_velocity;
-  path.y2 = path.y1 + y_velocity;
+  path_.x1 = origin_.x;
+  path_.y1 = origin_.y;
+  path_.x2 = vertex_.x;
+  path_.y2 = vertex_.y;
+  nearest_vertex_ = Distance(origin_, vertex_);
+  surface_ = kNone;
 
-  vertex.x = path.x2;
-  vertex.y = path.y2;
+  CheckCollision(paddle_, kPaddle);
+  CheckCollision(kScreenTop, kAxisX);
+  CheckCollision(kScreenLeft, kAxisY);
+  CheckCollision(kScreenRight, kAxisY);
 
-  if (Intersection(path, paddle_, &vertex)) {
-    surface = kPaddle;
-  }
-
-  if (Intersection(path, kScreenTop, &vertex)) {
-    surface = kAxisX;
-  }
-
-  if (Intersection(path, kScreenLeft, &vertex) ||
-      Intersection(path, kScreenRight, &vertex)) {
-    surface = kAxisY;
-  }
-
-  int dx{ vertex.x - path.x1 };
-  int dy{ vertex.y - path.y1 };
+  int dx{ path_.x2 - path_.x1 };
+  int dy{ path_.y2 - path_.y1 };
 
   ball->x_pos_ += dx;
   ball->y_pos_ += dy;
@@ -87,20 +94,26 @@ void Physics::ApplyVelocity(Ball *ball, int x_velocity, int y_velocity) {
   x_velocity = abs(dx) > abs(x_velocity) ? 0 : x_velocity - dx;
   y_velocity = abs(dy) > abs(y_velocity) ? 0 : y_velocity - dy;
 
-  if (surface == kPaddle) {
-    int ball_center{ ball->x_pos_ + (Ball::kBallWidth / 2) };
-    int paddle_center{ paddle_.x1 + (Paddle::kPaddleWidth / 2) };
-    ball->x_vel_ = (ball_center - paddle_center) / 4;
-  }
-
-  if (surface == kAxisX || surface == kPaddle) {
-    ball->y_vel_ = -ball->y_vel_;
-    y_velocity = -y_velocity;
-  }
-
-  if (surface == kAxisY) {
-    ball->x_vel_ = -ball->x_vel_;
-    x_velocity = -x_velocity;
+  int ball_center{ };
+  int paddle_center{ };
+  switch (surface_) {
+    case kAxisX:
+      ball->y_vel_ = -ball->y_vel_;
+      y_velocity = -y_velocity;
+      break;
+    case kAxisY:
+      ball->x_vel_ = -ball->x_vel_;
+      x_velocity = -x_velocity;
+      break;
+    case kPaddle:
+      ball_center = ball->x_pos_ + (Ball::kBallWidth / 2);
+      paddle_center = paddle_.x1 + (Paddle::kPaddleWidth / 2);
+      ball->x_vel_ = (ball_center - paddle_center) / 4;
+      ball->y_vel_ = -ball->y_vel_;
+      y_velocity = -y_velocity;
+      break;
+    default:
+      break;
   }
 
   ApplyVelocity(ball, x_velocity, y_velocity);
--- src/entity/physics.h
+++ src/entity/physics.h
@@ -34,12 +34,24 @@ class Physics {
   void Update(Ball *ball, const Paddle& paddle);
 
  private:
+  // Returns the distance between two points.
+  double Distance(const Point& a, const Point& b);
+
   // Retuns whether two lines intersect and records the vertex if they do.
-  bool Intersection(const Line& a, const Line& b, Point *vertex);
+  bool Intersection(const Line& a, const Line& b);
+
+  // Checks for a collision between the Ball's path and the bound, updating,
+  // the relevant members if the collision is the nearest so far.
+  void CheckCollision(const Line& bound, Collision type);
 
   // Moves the Ball along it's path, adjusting for collisions.
   void ApplyVelocity(Ball *ball, int x_velocity, int y_velocity);
 
+  Point origin_{ };
+  Point vertex_{ };
+  Line path_{ };
+  double nearest_vertex_{ };
+  Collision surface_{ };
   Line paddle_{ };
 };
 
