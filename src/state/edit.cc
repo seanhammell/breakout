4e9d753dc98004ac040a1506243935d777348bdd
Sean Hammell <sean.m.hammell@gmail.com>
2023-05-19 10:21:21 -0500

    Add brick zone outline

--- src/state/edit.cc
+++ src/state/edit.cc
@@ -1,6 +1,10 @@
 #include "src/state/edit.h"
 
+#include "SDL2/SDL.h"
+
+#include "src/game.h"
 #include "src/entity/brick.h"
+#include "src/graphic/renderer.h"
 
 Edit::Edit(const char *level) {
   if (Brick::LoadBricks(&bricks_, level)) {
@@ -9,6 +13,11 @@ Edit::Edit(const char *level) {
 }
 
 void Edit::Render() {
+  SDL_SetRenderDrawColor(kGame.renderer.get_renderer(), 0xEC, 0xEF, 0xF4, 0xFF);
+  SDL_Rect zone{ 1, 19, Renderer::kVirtualWidth - 1,
+                 Brick::kBrickHeight * 8 + 1 };
+  SDL_RenderDrawRect(kGame.renderer.get_renderer(), &zone);
+
   for (auto brick : bricks_) {
     brick.Render();
   }
