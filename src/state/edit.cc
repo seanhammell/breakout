c32bdfa4ff2e8182ac74ef2885b6d7c76fa3c764
Sean Hammell <sean.m.hammell@gmail.com>
2023-05-19 14:35:59 -0500

    Add brick placing

--- src/state/edit.cc
+++ src/state/edit.cc
@@ -17,27 +17,44 @@ Edit::Edit(const char *level)
 }
 
 void Edit::HandleInput(SDL_Event input) {
-  if (input.type == SDL_MOUSEMOTION) {
-    int x, y;
-    SDL_GetMouseState(&x, &y);
-    x /= 4;
-    y /= 4;
+  int x{ };
+  int y{ };
 
-    if (x < kZone.x || x > kZone.x + kZone.w ||
-        y < kZone.y || y > kZone.y + kZone.h) {
-      in_zone_ = false;
-      return;
-    }
+  SDL_GetMouseState(&x, &y);
 
-    in_zone_ = true;
-    hover_.set_x_pos(x - (x % Brick::kBrickWidth) + 1);
-    hover_.set_y_pos(y - (y % Brick::kBrickHeight));
-  } else if (input.type == SDL_MOUSEWHEEL) {
-    if (input.wheel.y > 0) {
-      hover_.set_type(std::min(hover_.get_type() + 1, +Brick::kRed));
-    } else if (input.wheel.y < 0) {
-      hover_.set_type(std::max(hover_.get_type() - 1, +Brick::kYellow));
-    }
+  x /= 4;
+  y /= 4;
+
+  int index{ };
+  switch (input.type) {
+    case SDL_MOUSEMOTION:
+
+      if (x <= kZone.x || x >= kZone.x + kZone.w ||
+          y <= kZone.y || y >= kZone.y + kZone.h) {
+        in_zone_ = false;
+        return;
+      }
+
+      in_zone_ = true;
+      hover_.set_x_pos(x - (x % Brick::kBrickWidth) + 1);
+      hover_.set_y_pos(y - (y % Brick::kBrickHeight));
+      break;
+    case SDL_MOUSEWHEEL:
+      if (input.wheel.y > 0) {
+        hover_.set_type(std::min(hover_.get_type() + 1, +Brick::kRed));
+      } else if (input.wheel.y < 0) {
+        hover_.set_type(std::max(hover_.get_type() - 1, +Brick::kYellow));
+      }
+      break;
+    case SDL_MOUSEBUTTONDOWN:
+      if (input.button.button == SDL_BUTTON_LEFT) {
+        index = (x / Brick::kBrickWidth) +
+                (((y - 20) / Brick::kBrickHeight) * 22);
+        bricks_[index] = hover_;
+        break;
+      }
+    default:
+      break;
   }
 }
 
